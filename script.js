c2b14095856cee6ef9249245edfe98b1c1c56a90
Rex Flores <Rexgflores@gmail.com>
2020-12-14 19:57:35 -0800

    Added timer, progressing end quiz functions and score/penalty functions

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 var startBtn = document.querySelector(".start")
-// Clicking Stat Quiz button to ask first question, deactivates display on start Screen, add to firstq div.
-startBtn.addEventListener("click", function(){
-  var startScrn = document.querySelector(".start-screen");
-  var quest1 = document.querySelector(".firstq")
-  startScrn.classList.remove("active");
-  quest1.classList.add("active")
+var timerScrn = document.querySelector("#timer-screen")
+var time= 10
+var timer
+var scorescreen = document.querySelector(".score-screen")
 
+// Clicking Stat Quiz button to ask first question, deactivates display on start Screen, add to firstq div.
+
+startBtn.addEventListener("click", function(){
+var startScrn = document.querySelector(".start-screen");
+var quest1 = document.querySelector(".firstq")
+  
+startScrn.classList.remove("active");
+  
+quest1.classList.add("active")
+  
+timerScrn.classList.add("active")
+  
+timer=setInterval(function(){
+timerScrn.textContent=time
+time --
+    if (time===0){
+    endquiz()
+      // TODO:End quiz
+    }
+  },1000)
+
+// TODO: add timer that counts down from 10 secs on click and disappears when time is up
 })
+// End game function
+function endquiz(){
+clearInterval(timer)
+document.querySelector(".timer-screen").classList.remove("active")
+// TODO:if the score-screen does not have the active element class, remove active tag and add it to score screen
+
+if (activeScrn!==".score-screen"){
+  
+  document.querySelector(".score-screen").classList.add("active")
+}
+}
 
 var answerSels = document.querySelectorAll("li")
 // The answer li's become an array as answerSels. Keep running loop while the index is less than the amount in array.
@@ -17,23 +48,29 @@ for (let index = 0; index < answerSels.length; index++) {
   selAnswer.addEventListener("click", function () {
     
     if (!this.classList.contains("correct") ){
-      // TODO:Add timer penalty for picking wrong answer
+    // TODO:Add timer penalty for picking wrong answer
     }
     // Removes active class from current "screen" then applies it to the next sibling element (div) onthe HTML 
     var activeScrn = document.querySelector(".active")
     activeScrn.classList.remove("active")
     activeScrn.nextElementSibling.classList.add("active")
-    
+    // timerScrn.classList.remove("active")
+    if (document.querySelector(".score-screen").classList.contains("active")){
+      endquiz()
+    }
   })
 }
+
 var enterNm = document.querySelector (".enterNm")
 enterNm.addEventListener("click", function(){
 
-  var initials = document.querySelector("#initials").value
- var highscores = document.querySelector(".high-scores")
- 
- var highScoreItem = document.createElement("li")
+var initials = document.querySelector("#initials").value
+var highscores = document.querySelector(".high-scores")
+var finalScore = time 
+var playerName= document.createElement("li")
 //  New list items will be created and added to high-scores as li elements
- highScoreItem.innerHTML = initials
- highscores.appendChild(highScoreItem)
+  playerName.innerHTML = initials
+   highscores.appendChild(playerName + time)
+//  TODO: Add player score alongside initials
+//  TODO: Save initials and highscores to local storage
 })
